07617e92372356de03654ac4c228310782daf77c
Eduardo Zimermam Pereira <eduardozimerman@live.com>
2019-05-16 11:35:08 -0300

    Término da implementação da API básica

--- apps/aluno.py
+++ apps/aluno.py
@@ -1,13 +1,33 @@
 # -*- coding: utf-8 -*-
 
-from flask import jsonify, request, app
+from flask import request
+from bson import json_util
 
 def aluno_api(app, collection):
 
-	@app.route('/', methods=['GET', 'POST'])
+	@app.route('/aluno', methods=['POST', 'DELETE'])
 	def aluno():
-		if request.method == 'GET':
-			return jsonify({'aluno': 'Eduardo Zimermam Pereria'})
-		
 		if request.method == 'POST':
-			return jsonify({'aluno': 'CRIADO'})
+			payload = request.get_json()
+
+			collection.insert_one(payload)
+
+			return json_util.dumps(payload)
+
+
+		if request.method == 'DELETE':
+
+			ra = request.args.get('ra')
+			campus = request.args.get('campus')
+			
+			try:
+
+				if (collection.find_one({'ra': ra, 'campus': campus})):
+					
+					collection.delete_one({'ra': ra, 'campus': campus})
+
+					return json_util.dumps({"status": "200", "data": "SUCESS"})
+				else:
+					return json_util.dumps({"status": "403", "data": "Documento não existente"})
+			except KeyError:
+				return json_util.dumps({"status": "404", "data": "Parâmetros errados"})
